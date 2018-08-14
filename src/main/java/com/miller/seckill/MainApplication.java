79e1f81ee1444e1ebaf3d5aef3eea8b21913f007
miller_xue <1021362065@qq.com>
2018-08-14 22:54:16 +0800

    打一个war

--- src/main/java/com/miller/seckill/MainApplication.java
+++ src/main/java/com/miller/seckill/MainApplication.java
@@ -4,8 +4,10 @@ import com.google.common.collect.Maps;
 import com.miller.seckill.filter.LoginFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
@@ -16,11 +18,15 @@ import java.util.Map;
  * Created by miller on 2018/8/6
  */
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }
 
     /*@Bean
     public FilterRegistrationBean filterRegistrationBean() {
